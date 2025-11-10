78cfef0429032abe9115b760b2f2042e667f10e0
Garvit1000 <garvitjoshi543@gmail.com>
2025-11-10 12:51:49 +0530

    Add fumadocs integration, docs pages, and .source folder to .gitignore.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import { RootProvider } from 'fumadocs-ui/provider/next';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,14 +29,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <RootProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </RootProvider>
       </body>
     </html>
   );
